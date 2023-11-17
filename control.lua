6af9fa734e7df7416cba99577fa27408b4dd4624
robot256 <robot256@GMAIL.COM>
2023-11-16 19:17:23 -0500

    Remake queue on setting change
    Ensure deconstruction queue is completely rebuilt when fluid or wire options are turned off, as well as on.  (So that not-yet-deconstructed miners with pipes or wires would be removed from the queue before they are deconstructed.)

--- control.lua
+++ control.lua
@@ -67,10 +67,8 @@ script.on_event(defines.events.on_runtime_mod_setting_changed, function(event)
     if not autodeconstruct.is_valid_pipe(settings.global[event.setting].value) then
       msg_all({"autodeconstruct-err-pipe-name", settings.global[event.setting].value})
     end
-  elseif ((event.setting == "autodeconstruct-remove-fluid-drills" and
-      settings.global['autodeconstruct-remove-fluid-drills'].value == true) or 
-      (event.setting == "autodeconstruct-remove-wired" and
-      settings.global['autodeconstruct-remove-wired'].value == true)) then
+  elseif (event.setting == "autodeconstruct-remove-fluid-drills" or 
+          event.setting == "autodeconstruct-remove-wired") then
     local _, err = pcall(autodeconstruct.init_globals)
     if err then msg_all({"autodeconstruct-err-generic", err}) end
   end
